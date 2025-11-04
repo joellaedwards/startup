57439c9ad899781859c0807142a3977bd9abb09c
joellaedwards <jmedw857@byu.edu>
2025-11-03 22:53:34 -0700

    outline of service/index

---
+++ service/index.js
@@ -0,0 +1,109 @@
+const cookieParse = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+let users = [];
+let games = [];
+
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+var apiRouter = expressRouter();
+app.use(`/api`, apiRouter);
+
+apiRouter.post('auth/creat', async(req, res) => {
+    if (await findUser('username', req.body.username)) {
+        req.status(409).send({msg: 'Existing user'});
+    } else {
+        const user = await createUser(req.body.username, req.body.password);
+
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username });
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('username', req.body.uwername);
+    if (user) {
+        if (await bycrypt.compare(req.body.password, user.password)) {
+            user.token = uuid.v4();
+            setAuthCookie(res, user.token);
+            res.send({username: user.username});
+            return;
+        }
+    }
+    res.setatus(401).send({ msg: 'Unauthorized'});
+});
+
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({msg: 'Unauthorized'});
+    }
+};
+
+apiRouter.get('/games', verifyAuth, (_req, res) => {
+    res.send(games)
+})
+
+apiRouter.post('/game', verifyAuth, (req, res) => {
+    scores = addGame(req.body);
+    res.send(scores);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+function addGame(newGame) {
+    games.push(newGame);
+    if (games.length > 10) {
+        games.length = 10;
+    }
+    return games;
+}
+
+
+async function findUser(field, value) {
+    if (!value) return null;
+    return users.find((u) => u[field] === value);
+}
+
+async function createUser(username, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        username: username,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+
+    return user;
+}
+
